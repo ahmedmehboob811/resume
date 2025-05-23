d8b5cd8cff21226b9797612af5e4ae1a5805f727
Mehboob Ahmed <160405430+ahmedmehboob811@users.noreply.github.com>
2025-05-23 23:47:25 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="text-gray-600 body-font p-6">
+          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+              </svg>
+              <span className="ml-3 text-3xl">Resume Builder</span>
+            </a>
+            <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-evenly text-2xl">
+              <a className="mr-5 hover:text-gray-900">Home</a>
+              <a className="mr-5 hover:text-gray-900">About</a>
+              <a className="mr-5 hover:text-gray-900">Contact</a>
+              <a className="mr-5 hover:text-gray-900">FAQ</a>
+            </nav>
+           
+          </div>
+        </header>
         {children}
       </body>
     </html>
